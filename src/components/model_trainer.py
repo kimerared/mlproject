b5be6f609d99e465fa88aac9e9ea70d0a66f7698
kimerared <kimerared84@gmail.com>
2024-03-16 08:11:51 -0400

    adding hyper-parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,7 +48,56 @@ class ModelTrainer:
                 'AdaBoost Regressor': AdaBoostRegressor(),
             }
             
-            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            # ---HYPER PARAMETERS---
+            params={
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor": {
+                'n_neighbors': [3, 5, 7, 9, 11, 13, 15],  # Number of neighbors to use
+                'weights': ['uniform', 'distance'],  # Weight function used in prediction
+                'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],  # Algorithm to use for computation
+                'leaf_size': [10, 20, 30, 40, 50],  # Leaf size passed to the tree or brute force algorithm
+                'p': [1, 2]  # Power parameter for the Minkowski metric
+                },
+                "XGB Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoost Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+                
+            }
+
+            # ---BEST MODEL---
+            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, 
+            models=models, param=params)
 
             best_model_score = max(model_report.values())
             best_model_name = max(model_report, key=model_report.get)
