e33c6e21c60fe342a22fef7ac8958d0c706e3e39
kimerared <kimerared84@gmail.com>
2024-03-17 12:38:10 -0400

    adding prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -22,7 +22,7 @@ from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path: str = os.path.join('artifacts', 'model.plk')
+    trained_model_file_path: str = os.path.join('artifacts', 'model.pkl')
 
 class ModelTrainer:
     def __init__(self):
